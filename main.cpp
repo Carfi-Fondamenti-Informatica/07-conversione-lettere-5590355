ebd5a81dba92ccdbaa1b18e6e59294ece51f1e94
Stefanomarelli <117829913+5590355@users.noreply.github.com>
2022-12-08 16:23:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,11 @@ using namespace std;
 int main() {
     char a;
     cin >> a;
-
-
-    if(convert(a)){
-        cout << a << endl;
-
-    }else{
-        cout << "errore" << endl;
+    if (funzione(a)== 1 ){
+        cout<< a <<endl;
+    }else {
+        cout<< "errore"<< endl;
     }
+
     return 0;
 }
